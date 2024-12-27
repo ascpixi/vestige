081b21511d689341a3261285ef81486be71ec4cf
ascpixi <44982772+ascpixi@users.noreply.github.com>
2024-12-27 23:27:34 +0100

    feat: increase responsiveness

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
+import * as tone from "tone";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "./index.css";
 
 import App from "./App.tsx";
+
+tone.setContext(new tone.Context({
+  clockSource: "timeout",
+  latencyHint: "playback",
+  updateInterval: 0.05 / 2
+}));
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
